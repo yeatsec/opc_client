2bc245800270d158730655382ba2867e0d922acd
Eric Yeats <eric.c.yeats@vanderbilt.edu>
2017-11-28 20:30:38 -0500

    added rgb_strip struct compatibility

--- opc_client.c
+++ opc_client.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <pthread.h>
+#include <strings.h>
 
 const int MAX_MSG_SIZE = 772; // 256 * 3 + 4
 
@@ -44,7 +44,7 @@ int opc_client_init(int _port, char * _server_address)
 	return 0;
 }
 
-int opc_client_send_formatted(char _channel, char _command, int _data_size, rgb * _data)
+int opc_client_send_formatted(char _channel, char _command, rgb_strip * _strip_ptr)
 {
 	if (!initialized)
 	{
@@ -54,19 +54,19 @@ int opc_client_send_formatted(char _channel, char _command, int _data_size, rgb 
 	char msg[MAX_MSG_SIZE];
 	msg[0] = channel;
 	msg[1] = command;
-	msg[2] = (_data_size * 3)/256;
-	msg[3] = (_data_size * 3)%256;
+	msg[2] = (_strip_ptr->size * 3)/256;
+	msg[3] = (_strip_ptr->size * 3)%256;
 
 	for (int i = 0; i < _data_size; ++i)
 	{
 		int base_index = 3*i + 4;
-		msg[base_index] = _data[i].red;
-		msg[base_index + 1] = _data[i].green;
-		msg[base_index + 2] = _data[i].blue;
+		msg[base_index] = _(_strip_ptr->rgb_leds)[i].red;
+		msg[base_index + 1] = (_strip_ptr->rgb_leds)[i].green;
+		msg[base_index + 2] = (_strip_ptr->rgb_leds)[i].blue;
 	}
 
 	// sendto
-	if (sendto(sock_fd, msg, 4+(3*_data_size), 0,(struct sockaddr *)&sa, sizeof(struct sockaddr_in6)) < 0)
+	if (sendto(sock_fd, msg, 4+(3*(_strip_ptr->size)), 0,(struct sockaddr *)&sa, sizeof(struct sockaddr_in6)) < 0)
 	{
 		printf("datagram sendto failed!\n");
 		return 2;
@@ -89,5 +89,23 @@ int opc_client_close()
 	return 0;
 }
 
+void opc_client_rgb_strip_init(rgb_strip * _strip, int _size)
+{
+	_strip->size = _size;
+	_strip->rgb_leds = (rgb *)malloc(_size*sizeof(rgb));
+	for (int i = 0; i < _size; ++i)
+	{
+		rgb_leds[i].red=0;
+		rgb_leds[i].green=0;
+		rgb_leds[i].blue=0;
+	}
+	return 0;
+}
+
+void opc_client_rgb_strip_destroy(rgb_strip * _strip)
+{
+	free(_strip->rgb_leds);
+	_strip->size = 0;
+}
 
 
--- opc_client.h
+++ opc_client.h
@@ -12,10 +12,19 @@ typedef struct rgb_struct {
 	char blue;
 } rgb;
 
+typedef struct rgb_strip_struct {
+	rgb rgb_leds[];
+	int size;
+} rgb_strip;
+
 int opc_client_init(int _port, char * _server_address);
 
-int opc_client_send_formatted(char _channel, char _command, int _data_size, rgb * _data);
+int opc_client_send_formatted(char _channel, char _command, rgb_strip * _strip_ptr);
 
 int opc_client_close();
+
+void opc_client_rgb_strip_init(rgb_strip * _strip, int _size);
+
+void opc_client_rgb_strip_destroy(rgb_strip * _strip);
 
 #endif	// _OPC_CLIENT_H_
